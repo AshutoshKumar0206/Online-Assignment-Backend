7aa47d9f4dfd0d4f04acedcefba6e859d9ee528e
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2024-12-21 16:02:38 +0530

    Added chat functionality from backend side

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ require("dotenv").config();
 const connectDB = require("./config/mongodb");
 const indexRoutes = require("./routes/index.route");
 const userRoutes = require("./routes/user.routes");
+const PORT = process.env.PORT || 4000;
 
+//To initialize a server
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(express.static('public'))
+
+//MongoDb Connection
 connectDB();
 
 app.use(express.json());
@@ -13,6 +21,36 @@ app.use(express.urlencoded({extended: true}));
 app.use("/", indexRoutes);
 app.use("/user", userRoutes);
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+const users = {};//To store active users and select the users to chat with
+io.on('connection', (socket) => {
+console.log('user connected:', socket.id);
+
+socket.on('join', ({ email, role }) => {
+    users[socket.id] = { email, role };
+    console.log(`${email} joined as ${role}`);
+});
+
+socket.on('sendMessage', ({ sender, receiver, message }) => {
+    const recipientSocketId = Object.keys(users).find(
+        (key) => users[key].username === receiver
+    );
+
+    if (recipientSocketId) {
+        io.to(recipientSocketId).emit('receivedMessage', {
+            sender,
+            message,
+        });
+    } else {
+        socket.emit('error', 'User not available.');
+    }
+});
+    //To disconnect the chat
+    socket.on('disconnect', () => {
+        console.log('User had disconnected:', users[socket.id]?.username);
+        delete users[socket.id];
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 })
