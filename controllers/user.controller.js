5503a7e2f990129c44afc0e890afc7ab991e0d5c
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2025-01-03 19:47:14 +0530

    Added Profile and updateProfile functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -463,61 +463,6 @@ module.exports.updateProfile = async (req, res, next) => {
       });
   }  
 }
-// Controller for Changing Password
-// exports.changePassword = async (req, res) => {
-//   try {
-//     const userDetails = await userModel.findById(req.user.id)
-
-//     const { oldPassword, newPassword } = req.body
-
-//     const isPasswordMatch = await bcrypt.compare(
-//       oldPassword,
-//       userDetails.password
-//     )
-//     if (!isPasswordMatch) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "The password is incorrect" })
-//     }
-
-//     // Update password
-//     const encryptedPassword = await bcrypt.hash(newPassword, 10)
-//     const updatedUserDetails = await userModel.findByIdAndUpdate(
-//       req.user.id,
-//       { password: encryptedPassword },
-//       { new: true }
-//     )
-
-//     // Send notification email
-//     try {
-//       const emailResponse = await mailSender(
-//         updatedUserDetails.email,
-//         "Password for your account has been updated",
-//         passwordUpdated(
-//           updatedUserDetails.email,
-//           `Password updated successfully for ${updatedUserDetails.firstName} ${updatedUserDetails.lastName}`
-//         )
-//       )
-//       console.log("Email sent successfully:", emailResponse.response)
-//     } catch (error) {
-//       console.error("Error occurred while sending email:", error)
-//       return res.status(500).json({
-//         success: false,
-//         message: "Error occurred while sending email",
-//         error: error.message,
-//       })
-//     }
-
-//     return res.status(200).json({ success: true, message: "Password updated successfully" })
-//   } catch (error) {
-//     console.error("Error occurred while updating password:", error)
-//     return res.status(500).json({
-//       success: false,
-//       message: "Error occurred while updating password",
-//       error: error.message,
-//     })
-//   }
-// }
 
 
 
