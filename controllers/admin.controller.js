b58aa9d87184b1f34fa67a4af4fd88f46de6613a
SHASHI SHANKAR <sk7698323@gmail.com>
2025-01-24 02:06:55 +0530

    user deletion synchronized

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -209,13 +209,43 @@ module.exports.getUser = async (req, res, next) => {
 module.exports.deleteUser = async (req, res, next) => {
   try {
     const userId = req.params.id;
+    console.log("userId", userId);
 
-    // Find and delete the user by ID
-    const deletedUser = await userModel.findByIdAndDelete(userId);
+    // Find the user by ID
+    const user = await userModel.findById(userId);
 
-    if (!deletedUser) {
+    if (!user) {
       return res.status(404).json({ success: false, message: "User not found" });
     }
+    console.log("user", user);
+
+    // Remove the user from all subjects they are enrolled in
+    await Subject.updateMany(
+      { students_id: userId },
+      { $pull: { students_id: userId } }
+    );
+    console.log("subjects found");
+
+    // If the user is a teacher, delete all subjects created by them
+    if (user.role === 'teacher') {
+      const subjects = await Subject.find({ teacher_id: userId });
+      console.log("subject deletion in progress");
+
+      for (const subject of subjects) {
+        // Delete all assignments and submissions related to the subject
+        await Assignment.deleteMany({ subjectId: subject._id });
+        await Submission.deleteMany({ assignmentId: { $in: subject.assignments_id } });
+console.log("assignment deleted");
+        // Delete the subject
+        await Subject.findByIdAndDelete(subject._id);
+      }
+    }
+
+    // Delete all assignments submitted by the user
+    await Submission.deleteMany({ studentId: userId });
+
+    // Delete the user
+    const deletedUser = await userModel.findByIdAndDelete(userId);
 
     res.status(200).json({
       success: true,
